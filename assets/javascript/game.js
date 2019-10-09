3f37860ff283bf095055db480f78b930d1e6c7ac
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-10-09 17:11:34 -0400

    reset function has been added. Struggling with getting the math.random() to display the same number once.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var wins = 0;
 var losses = 0;
-var userScore = $('#user-score');
+var userScore = $('#user-score').text(0);
 var lockNumber = false;
 
 
@@ -9,6 +9,20 @@ var lockNumber = false;
 var randomNumber = $('.random-number').text(Math.floor(Math.random() * (120 - 19) + 1) + 19);
 
 
+function reset() {
+    // set the wins to 0
+    wins = 0;
+
+    // set the losses to 0
+    losses = 0;
+
+    // set the user score counter to 0
+    $('#user-score').text(0);
+
+    // generate a new random number
+    randomNumber = $('.random-number').text(Math.floor(Math.random() * (120 - 19) + 1) + 19);
+}
+
 // crystal img click events
 
 $('#red-crystal').on('click', function() {
@@ -16,42 +30,55 @@ $('#red-crystal').on('click', function() {
     // random number generator for crystal img from 1 -12
     var randomImgNumber = Math.floor(Math.random() * 12) + 1;
 
-    randomImgNumber += randomImgNumber;
+
 
     $(userScore).text(randomImgNumber);
 
-    if (userScore > randomNumber) {
+
+    if (userScore === randomNumber) {
         wins++;
-        $('wins').text(wins);
+        $('#wins').text(wins);
+
+        // Display a message that tells the user he won!
+        // $('.scoreboard').text("Congrats! You won!!");
 
         // reset function
         // reset();
+
+    } else if (userScore > randomNumber) {
+        losses++;
+        $('#losses').text(losses);
+
+        // Display a message that tells the user he won!
+        // $('.scoreboard').text("HA! You Lost...");
+
+        // reset
     }
 
     // test
     console.log("Red crystal: " + randomImgNumber);
 });
 
-$('#green-crystal').on('click', function() {
-    // random number generator for crystal img from 1 -12
-    var randomImgNumber = Math.floor(Math.random() * 12) + 1;
+// $('#green-crystal').on('click', function() {
+//     // random number generator for crystal img from 1 -12
+//     var randomImgNumber = Math.floor(Math.random() * 12) + 1;
 
-    // test
-    console.log("Green crystal: " + randomImgNumber);
-});
+//     // test
+//     console.log("Green crystal: " + randomImgNumber);
+// });
 
-$('#yellow-crystal').on('click', function() {
-    // random number generator for crystal img from 1 -12
-    var randomImgNumber = Math.floor(Math.random() * 12) + 1;
+// $('#yellow-crystal').on('click', function() {
+//     // random number generator for crystal img from 1 -12
+//     var randomImgNumber = Math.floor(Math.random() * 12) + 1;
 
-    // test
-    console.log("Yellow crystal: " + randomImgNumber);
-});
+//     // test
+//     console.log("Yellow crystal: " + randomImgNumber);
+// });
 
-$('#blue-crystal').on('click', function() {
-    // random number generator for crystal img from 1 -12
-    var randomImgNumber = Math.floor(Math.random() * 12) + 1;
+// $('#blue-crystal').on('click', function() {
+//     // random number generator for crystal img from 1 -12
+//     var randomImgNumber = Math.floor(Math.random() * 12) + 1;
 
-    // test
-    console.log("Blue crystal: " + randomImgNumber);
-});
+//     // test
+//     console.log("Blue crystal: " + randomImgNumber);
+// });
