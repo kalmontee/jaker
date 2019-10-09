5b70682edf3a7b112c80ae485e4bf6fc5529ee6e
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-10-09 13:20:01 -0400

    Added IDs to each crystal img to manipulate it with javascript.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 var wins = 0;
 var losses = 0;
 var userScore = $('#user-score');
+var lockNumber = false;
 
-// randomNumber variable will target the class div "random-number"
-var randomNumber = $('.random-number');
 
 // Generate a random number from 19 - 120 (max - min).
 // 120 is the max number so we subtract it to 19 which is the min number
-$(randomNumber).text(Math.floor(Math.random() * (120 - 19) + 1) + 19);
+var randomNumber = $('.random-number').text(Math.floor(Math.random() * (120 - 19) + 1) + 19);
+
 
 // crystal img click events
 $('.crystal-img').on("click", function() {
@@ -15,8 +15,16 @@ $('.crystal-img').on("click", function() {
     // random number generator for crystal img from 1 -12
     var randomImgNumber = Math.floor(Math.random() * 12) + 1;
 
+    // click 3 + n = 3;
+
+    // random number generator to stay the same unless the page is refresh.
+    if (randomImgNumber !== lockNumber) {
+
+    }
+
+    // output the random number generator to userScore
+
+
     // test
     console.log(randomImgNumber);
 });
-
-var userScore = $('#user-score');
