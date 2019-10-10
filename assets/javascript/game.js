058c37be92b52074c9285d4ab92f2c051fd0c068
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-10-09 22:01:24 -0400

    Wins and losses has been added to each crystal click event function. Encountered a bug where each crystal is being added twice the result. Having major issues to fix it. Lastly, few minor changes in the code.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
 var wins = 0;
 var losses = 0;
-var userScore = $('#user-score').text(0);
-var lockNumber = false;
+var userScore = $('#user-score').text(userScore);
 
 
 // Generate a random number from 19 - 120 (max - min).
 // 120 is the max number so we subtract it to 19 which is the min number
-var randomNumber = $('.random-number').text(Math.floor(Math.random() * 101) + 19);
+var randomNumber = Math.floor(Math.random() * 101) + 19;
+$('.random-number').text(randomNumber);
 
 // random number generator for crystal img from 1 -12
 var redCrystal = Math.floor(Math.random() * 12) + 1;
@@ -16,10 +16,12 @@ var blueCrystal = Math.floor(Math.random() * 12) + 1;
 
 function reset() {
     // set the user score counter to 0
-    $('#user-score').text(0);
+    userScore = 0;
+    $('#user-score').text(userScore);
 
     // generate a new random number
-    randomNumber = $('.random-number').text(Math.floor(Math.random() * (120 - 19) + 1) + 19);
+    randomNumber = Math.floor(Math.random() * 101) + 19;
+    $('.random-number').text(randomNumber);
 
     // random number generator for crystal img from 1 -12
     var redCrystal = Math.floor(Math.random() * 12) + 1;
@@ -33,8 +35,27 @@ $('#red-crystal').on('click', function() {
     // output the random number crystal to the user score
     $(userScore).text(redCrystal);
 
-    // add the redCrystal random number to the user score
+    // add the redCrystal random number to the user score (counter)
     redCrystal += redCrystal;
+
+    // userScore === randomNumber ? win() : lose();
+
+    if (redCrystal === randomNumber) {
+        // add +1 to wins
+        wins++;
+        $('#wins').text(wins);
+
+        // When won call the reset function
+        reset();
+
+    } else if (redCrystal > randomNumber) {
+        // add +1 to losses
+        losses++;
+        $('#losses').text(losses);
+
+        // When lost call the reset function
+        reset();
+    }
 
     // test
     console.log("Red crystal: " + redCrystal);
@@ -47,6 +68,23 @@ $('#green-crystal').on('click', function() {
     // add the redCrystal random number to the user score
     greenCrystal += greenCrystal;
 
+    if (redCrystal === randomNumber) {
+        // add +1 to wins
+        wins++;
+        $('#wins').text(wins);
+
+        // When won call the reset function
+        reset();
+
+    } else if (redCrystal > randomNumber) {
+        // add +1 to losses
+        losses++;
+        $('#losses').text(losses);
+
+        // When lost call the reset function
+        reset();
+    }
+
     // test
     console.log("Green crystal: " + greenCrystal);
 });
@@ -58,6 +96,23 @@ $('#yellow-crystal').on('click', function() {
     // add the redCrystal random number to the user score
     yellowCrystal += yellowCrystal;
 
+    if (redCrystal === randomNumber) {
+        // add +1 to wins
+        wins++;
+        $('#wins').text(wins);
+
+        // When won call the reset function
+        reset();
+
+    } else if (redCrystal > randomNumber) {
+        // add +1 to losses
+        losses++;
+        $('#losses').text(losses);
+
+        // When lost call the reset function
+        reset();
+    }
+
     // test
     console.log("Yellow crystal: " + yellowCrystal);
 });
@@ -68,6 +123,23 @@ $('#blue-crystal').on('click', function() {
 
     // add the redCrystal random number to the user score
     blueCrystal += blueCrystal;
+
+    if (redCrystal === randomNumber) {
+        // add +1 to wins
+        wins++;
+        $('#wins').text(wins);
+
+        // When won call the reset function
+        reset();
+
+    } else if (redCrystal > randomNumber) {
+        // add +1 to losses
+        losses++;
+        $('#losses').text(losses);
+
+        // When lost call the reset function
+        reset();
+    }
 
     // test
     console.log("Blue crystal: " + blueCrystal);
